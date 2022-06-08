f83c2a183bbea03a9a1c8114748ef9d6814af7cb
Rene <renesuar99@gmail.com>
2022-06-08 17:33:38 -0400

    Created ui and logic

--- script.js
+++ script.js
@@ -4,6 +4,62 @@ let playerScore = 0;
 let gameOutcome = "";
 let playerChoice = ""; 
 
+const container = document.querySelector('#container');
+
+const score = document.createElement('p');
+score.classList.add('score');
+score.textContent = `Your score: ${playerScore}  Computer's Score: ${computerScore}`; 
+container.appendChild(score);
+
+
+
+const rock = document.createElement('button');
+rock.textContent = "Rock";
+rock.classList.add("rock");
+container.appendChild(rock);
+rock.addEventListener('click', function (e)
+{
+playerChoice = "rock";
+
+playRound();
+
+});
+
+const paper = document.createElement('button');
+paper.textContent = "Paper";
+paper.classList.add("paper");
+container.appendChild(paper);
+paper.addEventListener('click', function (e) {
+
+playerChoice = 'paper';
+playRound();
+
+});
+
+
+
+const scissors = document.createElement('button');
+scissors.textContent = "Scissors";
+scissors.classList.add("scissors");
+container.appendChild(scissors);
+scissors.addEventListener('click', function (e) {
+playerChoice = "scissors"; 
+
+playRound();
+
+
+});
+
+const choicesPicked = document.createElement('p');
+choicesPicked.classList.add('choicesPicked');
+choicesPicked.textContent = ``; 
+container.appendChild(choicesPicked);
+
+
+
+
+
+
 // Generates a random number between 1-3 that will represent the computers choice 
 function computerPlay () { 
 computerChoice = Math.floor(Math.random() * 3 + 1)
@@ -27,27 +83,26 @@ if (computerChoice === 3) {
 
 
 
-function playRound (playerChoice, computerChoice) { 
 
 
-playerSelection = prompt("Enter rock, paper, or scissors.");
 
-playerChoice = playerSelection.toLowerCase();
+// playerChoice, computerChoice
+function playRound () { 
 
 
+
+
+computerPlay();
+console.log(computerChoice);
 if (playerChoice === computerChoice) {
 
     console.log("It was a tie!");
     playerScore++
-    computerScore++
-}
+    computerScore++ }
 
 
 
 
-
-
-// outcomes if player picks rock 
 
 if (playerChoice === "rock" && computerChoice === "paper")
 {
@@ -90,27 +145,71 @@ if (playerChoice === "scissors" && computerChoice === "paper")
     gameOutcome =  console.log(`You win! computer picked ${computerChoice}`);
     playerScore++
 }
- return playerChoice, computerChoice, gameOutcome;
+
+score.textContent = `Your score: ${playerScore}  Computer's Score: ${computerScore}`; 
+choicesPicked.textContent = `You picked: ${playerChoice} - The computer picked: ${computerChoice}`; 
+
+
+
+
+
+
+if (playerScore === 5 && computerScore === 5)
+{
+score.textContent = `Wow you both tied! incredible!!!`;
+computerScore = 0; 
+playerScore = 0;
+}
+
+else if (computerScore === 5 ){
+ score.textContent = `You lost! The computer won ${computerScore} to ${playerScore}`;
+computerScore = 0; 
+playerScore = 0;
+}
+
+else if (playerScore === 5) {
+score.textContent = `Congratulations! You won! ${playerScore} to ${computerScore}`;
+computerScore = 0; 
+playerScore = 0;
+}
+ 
+else
+
+ return playerChoice, computerChoice, gameOutcome, playerScore, computerScore;
+ 
 }
 
 
-function game () { 
 
 
-    for (let i = 0; i < 5; i++) {
-        playRound(playerChoice, computerChoice)
-    }
 
-    if (playerScore > computerScore)
-    console.log(` You won! The score was ${playerScore} - ${computerScore} !`)
 
-    if (playerScore < computerScore)
-    console.log(` You lost! The score was ${playerScore} - ${computerScore} !`)
 
-    if (playerScore === computerScore) {
-        console.log(`It was a tie! ${playerScore} - ${computerScore}` )
-    }
-}
+
+
+
+
+
+
+
+
+// function game () { 
+
+
+//     for (let i = 0; i < 5; i++) {
+//         playRound(playerChoice, computerChoice)
+//     }
+
+//     if (playerScore > computerScore)
+//     console.log(` You won! The score was ${playerScore} - ${computerScore} !`)
+
+//     if (playerScore < computerScore)
+//     console.log(` You lost! The score was ${playerScore} - ${computerScore} !`)
+
+//     if (playerScore === computerScore) {
+//         console.log(`It was a tie! ${playerScore} - ${computerScore}` )
+//     }
+// }
 
 
 
